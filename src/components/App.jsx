ee91b6b7c03ab25f6023f54d1a4b3558672591ff
Illia Kazmin <kazmin1c@gmail.com>
2022-10-28 00:30:20 +0300

    move json into folder data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-import user from '../user.json'
-import dataStatistics from '../data.json';
-import friends from '../friends.json';
-import transctions from '../transactions.json'
+import user from '../data/user.json'
+import dataStatistics from '../data/data.json';
+import friends from '../data/friends.json';
+import transctions from '../data/transactions.json'
 
 import Profile from "./Profile/Profile";
 import Statistics from "./Statistics/Statistics";
